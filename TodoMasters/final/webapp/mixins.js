5fb10253c0c2a6a269596af02effcf56ca6a84f3
s4sachin <sachinpurohit.text@gmail.com>
2025-09-03 09:26:21 +0530

    Project setup

--- TodoMasters/final/webapp/mixins.js
+++
@@ -1,7 +0,0 @@
-// Observer Design Pattern
-export const observerMixin = {
-    observers: new Set(),
-    addObserver(obs) { this.observers.add(obs) },
-    removeObserver(obs) { this.observers.delete(obs)},
-    notify() { this.observers.forEach(obs=>obs()) }
-}
