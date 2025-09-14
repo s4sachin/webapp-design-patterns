a078dfa4368e6bd378109fac95a387165ab30219
s4sachin <sachinpurohit.text@gmail.com>
2025-09-15 02:23:41 +0530

    add: View Transition API pattern to make transitions beautiful along with some custom CSS for transitions

--- CoffeeMasters/src/services/Router.js
+++ CoffeeMasters/src/services/Router.js
@@ -6,14 +6,14 @@ const Router = {
                 const href = event.target.getAttribute("href");
                 Router.go(href);
             });
-        });  
+        });
         // It listen for history changes
         window.addEventListener('popstate',  event => {
             Router.go(event.state.route, false);
         });
         // Process initial URL   
         Router.go(location.pathname);
-    },    
+    },
     go: async (route, addToHistory=true) => {
         if (addToHistory) {
             history.pushState({ route }, '', route);
@@ -29,22 +29,29 @@ const Router = {
                 pageElement = document.createElement("order-page");
                 break;
             default:
-                if (route.startsWith("/product-")) {                
+                if (route.startsWith("/product-")) {
                     pageElement = document.createElement("details-page");
                     pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
                 }
-                break;   
+                break;
         }
         if (pageElement) {
-            // get current page element            
-            let currentPage = document.querySelector("main").firstElementChild; 
-            if (currentPage) {
-                currentPage.remove();
-                document.querySelector("main").appendChild(pageElement);
-            } else {
-                document.querySelector("main").appendChild(pageElement);
+            function changePage() {
+                // get current page element            
+                let currentPage = document.querySelector("main").firstElementChild;
+                if (currentPage) {
+                    currentPage.remove();
+                    document.querySelector("main").appendChild(pageElement);
+                } else {
+                    document.querySelector("main").appendChild(pageElement);
+                }
             }
-
+            if(document.startViewTransition){
+                document.startViewTransition(() => changePage());
+            }
+            else {
+                changePage();
+            }
         }
 
         window.scrollX = 0;
